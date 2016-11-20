e81b218e8f594e4529209825dcdf6be3b201166b
Luc Didry <luc@didry.org>
2016-11-20 22:12:29 +0100

    Adding milligram theme

---
+++ themes/milligram/lib/Lstu/I18N.pm
@@ -0,0 +1,16 @@
+# vim:set sw=4 ts=4 sts=4 ft=perl expandtab:
+package Lstu::I18N;
+
+use base 'Locale::Maketext';
+use File::Basename qw/dirname/;
+use Locale::Maketext::Lexicon {
+    _auto => 1,
+    _decode => 1,
+    _style  => 'gettext',
+    '*' => [
+        Gettext => dirname(__FILE__) . '/I18N/*.po',
+        Gettext => dirname(__FILE__) . '/../../../default/lib/Lstu/I18N/*.po',
+    ]
+};
+
+1;
